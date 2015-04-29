b822235ec3541838a949be0d2e0ed8b71f57256c
Victor Gaitour <victorgaitour@yahoo.com>
2015-04-29 13:03:26 -0400

    minor base

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,request,url_for,redirect,render_template,session,flash
+from auth import auth
+app=Flask(__name__)
+
+@app.route("/")
+def index:
+    return render_template("index.html")
+
+@app.route("/base")
+def base:
+    return render_template("base.html")
+
+@app.route("/login")
+def login:
+    return render_template("login.html")
+
+
+if __name__=="main":
+    app.debug=True
+    app.secret_key="Dankmemesbro"
+    app.run()
+
