d7ff3128cb93d3ef473eaf82fcaf77df9b2c0fb7
Matthew Cook <mcookhome@gmail.com>
2015-05-01 14:01:45 -0400

    register works

--- app.py
+++ app.py
@@ -1,18 +1,51 @@
 from flask import Flask,request,url_for,redirect,render_template,session,flash
+from utils import manager
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
+    print manager.getIDs()
     return render_template("index.html")
 
 @app.route("/base")
 def base():
     return render_template("base.html")
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    print "login"
+    return render_template("login.html", loggedin=False)
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    ids= manager.getIDs()
+    if 'username' in session:
+        loggedin=True
+        username=session['username']
+    else:
+        loggedin=False
+        username=''
+    if request.method=='POST':
+        if 'username' not in session:
+            username = request.form['username']
+            password = request.form['password']
+            reppassword = request.form['password2']
+            reason = ""
+            registered=False
+
+            if password == reppassword:
+                registered=True
+            else:
+                registered=False
+                reason = "Passwords do not match"
+                print "Passwords do not match"
+            manager.finishRegistration(username,password,registered)
+            if registered:
+                return render_template("register.html", page=1, username=username,ids=ids)
+        return render_template("register.html", page=2, reason=reason,ids=ids)
+    else:
+        return render_template("register.html", page=3, loggedin=loggedin, username=username, ids=ids) 
 
 @app.route("/canvas")
 def canvas():
