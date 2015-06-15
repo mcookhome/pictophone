4640937762cfabf39d442d1dfc1e13dd52f74bdc
Hubert <hubertpuszklewicz@gmail.com>
2015-06-15 01:49:46 -0400

    fixing match

--- scripts/matchalgorithm.py
+++ scripts/matchalgorithm.py
@@ -12,11 +12,19 @@ def match(original, typed):
     def add(): points += 1
     matchPts = len([w for w in origWords if w in typedWords])/length(1.0)
     excessPts = len([w for w in typedWords if w not in origWords])/length(2.0)
-    similarPts = [wn.synset(w) for w in origWords]
+    #similarPts = [wn.synset(w) for w in origWords]
     points = matchPts - excessPts
     print forNLTK
-    print similarPts
+    #print similarPts
     return points
+
+
+def lemmalist(str):
+    syn_set = []
+    for synset in wn.synsets(str):
+        for item in synset.lemma_names:
+            syn_set.append(item)
+    return syn_set
 
 
 def wordMatch(original, typed):
@@ -34,3 +42,8 @@ test2= "man riding a bike with a fruit"
 print match(test1, test2)
             
     
+syns = wn.synsets("man")
+for i in syns:
+    for x in i.lemma_names:
+        print x
+
